1358293e3c447deacfb409e4e96072952b79fffb
Nguyen Van  Manh <nguyentrungcongduong@gmail.com>
2024-11-09 10:48:29 +0700

    done

---
+++ Nhom8-SourceCode/Game Caro/GameCaro.cs
@@ -0,0 +1,480 @@
+﻿using System;
+using System.Drawing;
+using System.Messaging;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+using System.Security.Policy;
+using System.IO;
+
+namespace Game_Caro
+{
+    partial class GameCaro : Form
+    {
+        #region Properties
+        GameBoard board;
+        SocketManager socket;
+        string PlayerName;
+        SoundPlayer moveSound;
+        SoundPlayer winSound;
+
+        public GameCaro()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            
+            try
+            {
+                moveSound = new SoundPlayer("C:/Nhom8-SourceCode/Game Caro/Resources/move_sound.wav");
+                moveSound.Load(); // Tải âm thanh trước khi sử dụng
+
+                winSound = new SoundPlayer("C:/Nhom8-SourceCode/Game Caro/Resources/win_sound.wav");
+                winSound.Load(); // Tải âm thanh trước khi sử dụng
+            }
+            catch (FileNotFoundException fnfEx)
+            {
+                MessageBox.Show("Không tìm thấy tệp âm thanh: " + fnfEx.Message, "Lỗi tệp", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải âm thanh: " + ex.Message, "Lỗi âm thanh", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            board = new GameBoard(pn_GameBoard, txt_PlayerName, pb_Avatar);            
+            board.PlayerClicked += Board_PlayerClicked;
+            board.GameOver += Board_GameOver;
+
+            pgb_CountDown.Step = Constance.CountDownStep;
+            pgb_CountDown.Maximum = Constance.CountDownTime;
+
+            tm_CountDown.Interval = Constance.CountDownInterval;
+            socket = new SocketManager();
+
+            txt_Chat.Text = "";
+            btn_Send.Click += Btn_Send_Click;
+
+            NewGame();
+        }
+        #endregion
+
+        #region Methods
+
+        void NewGame()
+        {
+            pgb_CountDown.Value = 0;
+            tm_CountDown.Stop();
+
+            undoToolStripMenuItem.Enabled = true;
+            redoToolStripMenuItem.Enabled = true;
+
+            btn_Undo.Enabled = true;
+            btn_Redo.Enabled = true;
+
+            board.DrawGameBoard();
+        }
+
+        void EndGame()
+        {
+            undoToolStripMenuItem.Enabled = false;
+            redoToolStripMenuItem.Enabled = false;
+
+            btn_Undo.Enabled = false;
+            btn_Redo.Enabled = false;
+
+            tm_CountDown.Stop();
+            pn_GameBoard.Enabled = false;
+        }
+
+        private void GameCaro_Load(object sender, EventArgs e)
+        {
+            lbl_About.Text = "Chào mừng các bạn đến với trò chơi CARO";
+            tm_About.Enabled = true;
+        }
+
+        private void GameCaro_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc muốn thoát không", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != System.Windows.Forms.DialogResult.Yes)
+                e.Cancel = true;
+            else
+            {
+                try
+                {
+                    socket.Send(new SocketData((int)SocketCommand.QUIT, "", new Point()));
+                } catch { }
+            }
+        }
+
+        private void Board_PlayerClicked(object sender, BtnClickEvent e)
+        {
+            try
+            {
+                moveSound.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi phát âm thanh di chuyển: " + ex.Message);
+            }
+            tm_CountDown.Start(); 
+            pgb_CountDown.Value = 0;
+
+            if (board.PlayMode == 1)
+            {
+                try
+                {
+                    pn_GameBoard.Enabled = false;
+                    socket.Send(new SocketData((int)SocketCommand.SEND_POINT, "", e.ClickedPoint));
+
+                    undoToolStripMenuItem.Enabled = false;
+                    redoToolStripMenuItem.Enabled = false;
+
+                    btn_Undo.Enabled = false;
+                    btn_Redo.Enabled = false;
+
+                    Listen();
+                }
+                catch
+                {
+                    EndGame();
+                    MessageBox.Show("Không có kết nối nào tới máy đối thủ", "Lỗi kết nối", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Board_GameOver(object sender, EventArgs e)
+        {
+            EndGame();
+            try
+            {
+                winSound.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi phát âm thanh chiến thắng: " + ex.Message);
+            }
+
+            if (board.PlayMode == 1)
+                socket.Send(new SocketData((int)SocketCommand.END_GAME, "", new Point()));
+        }
+
+        private void Tm_CountDown_Tick(object sender, EventArgs e)
+        {
+            pgb_CountDown.PerformStep();
+
+            if (pgb_CountDown.Value >= pgb_CountDown.Maximum)
+            {
+                EndGame();
+
+                string winnerName = board.ListPlayers[board.CurrentPlayer == 1 ? 0 : 1].Name;
+
+                MessageBox.Show("Hết giờ rồi !!! " + winnerName + " đã chiến thắng ♥ !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (board.PlayMode == 1)
+                    socket.Send(new SocketData((int)SocketCommand.TIME_OUT, "", new Point()));
+            }                                    
+        }
+
+        private void Tm_About_Tick(object sender, EventArgs e)
+        {
+            lbl_About.Location = new Point(lbl_About.Location.X, lbl_About.Location.Y - 2);
+
+            if (lbl_About.Location.Y + lbl_About.Height < 0)
+                lbl_About.Location = new Point(lbl_About.Location.X, Grb_About.Height - 10);
+        }
+
+        #region MenuStrip
+        private void NewGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+
+            if (board.PlayMode == 1)
+            {
+                try
+                {
+                    socket.Send(new SocketData((int)SocketCommand.NEW_GAME, "", new Point()));
+                }
+                catch { }
+            }
+                
+            pn_GameBoard.Enabled = true;
+        }
+
+        private void UndoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pgb_CountDown.Value = 0;
+            board.Undo();
+
+            if (board.PlayMode == 1)
+                socket.Send(new SocketData((int)SocketCommand.UNDO, "", new Point()));
+        }
+
+        private void RedoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // pgb_CountDown.Value = 0;
+            board.Redo();
+
+            if (board.PlayMode == 1)
+                socket.Send(new SocketData((int)SocketCommand.REDO, "", new Point()));
+        }
+
+        private void QuitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ViaLANToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            board.PlayMode = 1;
+            NewGame();
+
+            socket.IP = txt_IP.Text;
+
+            if (!socket.ConnectServer())
+            {
+                socket.IsServer = true;
+                pn_GameBoard.Enabled = true;
+                socket.CreateServer();
+                MessageBox.Show("Bạn đang là Server", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                socket.IsServer = false;
+                pn_GameBoard.Enabled = false;
+                Listen();
+                MessageBox.Show("Kết nối thành công !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void SameComToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (board.PlayMode == 1)
+            {
+                try
+                {
+                    socket.Send(new SocketData((int)SocketCommand.QUIT, "", new Point()));
+                } catch { }
+
+                socket.CloseConnect();
+                MessageBox.Show("Đã ngắt kết nối mạng LAN", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            board.PlayMode = 2;
+            NewGame();
+        }
+
+        private void PlayerToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (board.PlayMode == 1)
+            {
+                if (board.PlayMode == 1)
+                {
+                    try
+                    {
+                        socket.Send(new SocketData((int)SocketCommand.QUIT, "", new Point()));
+                    } catch { }
+
+                    socket.CloseConnect();
+                    MessageBox.Show("Đã ngắt kết nối mạng LAN", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            board.PlayMode = 3;
+            NewGame();
+            board.StartAI();
+        }
+
+        private void SettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HowToPlayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ContactMeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutThisGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion     
+
+        #region Button Settings
+        private void Btn_LAN_Click(object sender, EventArgs e)
+        {
+            ViaLANToolStripMenuItem_Click(sender, e);
+        }
+
+        private void Btn_SameCom_Click(object sender, EventArgs e)
+        {
+            SameComToolStripMenuItem_Click(sender, e);
+        }
+
+        private void Btn_AI_Click(object sender, EventArgs e)
+        {
+            PlayerToolStripMenuItem1_Click(sender, e);
+        }
+
+        private void Btn_Undo_Click(object sender, EventArgs e)
+        {
+            UndoToolStripMenuItem_Click(sender, e);
+        }
+
+        private void Btn_Redo_Click(object sender, EventArgs e)
+        {
+            RedoToolStripMenuItem_Click(sender, e);
+        }
+
+        private void Btn_Send_Click(object sender, EventArgs e)
+        {
+            if (board.PlayMode != 1) // Kiểm tra chế độ chơi mạng
+                return;
+
+            string playerName = board.ListPlayers[socket.IsServer ? 0 : 1].Name; // Lấy tên người chơi
+            string message = txt_Message.Text.Trim(); // Lấy nội dung tin nhắn và loại bỏ khoảng trắng
+
+            if (!string.IsNullOrEmpty(message)) // Kiểm tra nếu tin nhắn không rỗng
+            {
+                txt_Chat.Text += $"- {playerName}: {message}\r\n"; // Cập nhật lịch sử chat
+                socket.Send(new SocketData((int)SocketCommand.SEND_MESSAGE, message, new Point())); // Gửi tin nhắn qua socket
+                txt_Message.Clear(); // Xóa nội dung nhập sau khi gửi
+            }
+        }
+        #endregion
+
+        #region LAN settings
+        private void GameCaro_Shown(object sender, EventArgs e)
+        {
+            txt_IP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Wireless80211);
+
+            if (string.IsNullOrEmpty(txt_IP.Text))
+                txt_IP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Ethernet);
+        }
+
+        private void Listen()
+        {
+            Thread ListenThread = new Thread(() =>
+            {
+                try
+                {
+                    SocketData data = (SocketData)socket.Receive();
+                    ProcessData(data);
+                }
+                catch { }
+            });
+
+            ListenThread.IsBackground = true;
+            ListenThread.Start();
+        }
+
+        private void ProcessData(SocketData data)
+        {
+            PlayerName = board.ListPlayers[board.CurrentPlayer == 1 ? 0 : 1].Name;
+
+            switch (data.Command)
+            {
+                case (int)SocketCommand.SEND_POINT:
+                    // Có thay đổi giao diện muốn chạy ngọt phải để trong đây
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        board.OtherPlayerClicked(data.Point);
+                        pn_GameBoard.Enabled = true;
+
+                        pgb_CountDown.Value = 0;
+                        tm_CountDown.Start();
+
+                        undoToolStripMenuItem.Enabled = true;
+                        redoToolStripMenuItem.Enabled = true;
+
+                        btn_Undo.Enabled = true;
+                        btn_Redo.Enabled = true;
+                    }));
+                    break;
+
+                case (int)SocketCommand.SEND_MESSAGE:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        txt_Chat.Text += data.Message + "\r\n"; // Cập nhật lịch sử chat với tin nhắn mới
+                    }));
+                    break;
+
+                case (int)SocketCommand.NEW_GAME:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        NewGame();
+                        pn_GameBoard.Enabled = false;
+                    }));
+                    break;
+
+                case (int)SocketCommand.UNDO:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        pgb_CountDown.Value = 0;
+                        board.Undo();
+                    }));
+                    break;
+
+                case (int)SocketCommand.REDO:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        // pgb_CountDown.Value = 0;
+                        board.Redo();
+                    }));
+                    break;
+
+                case (int)SocketCommand.END_GAME:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        EndGame();
+                        MessageBox.Show(PlayerName + " đã chiến thắng ♥ !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }));
+                    break;
+
+                case (int)SocketCommand.TIME_OUT:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        EndGame();
+                        MessageBox.Show("Hết giờ rồi !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }));
+                    break;
+
+                case (int)SocketCommand.QUIT:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        tm_CountDown.Stop();
+                        EndGame();
+                    
+                        board.PlayMode = 2;
+                        socket.CloseConnect();
+
+                        MessageBox.Show("Đối thủ đã chạy mất dép", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }));
+                    break;
+
+                default:
+                    break;
+            }
+
+            Listen();
+        }
+        #endregion
+
+        #endregion
+
+        private void menuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void txt_PlayerName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
